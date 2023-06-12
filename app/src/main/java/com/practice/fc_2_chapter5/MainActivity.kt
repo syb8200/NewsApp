818ce8b81446c3cf75046675322e4e6de091430d
syb8200 <syb8200@naver.com>
2023-06-12 15:41:16 +0900

    Chip 이용하여 상단 탭 구현

--- app/src/main/java/com/practice/fc_2_chapter5/MainActivity.kt
+++ app/src/main/java/com/practice/fc_2_chapter5/MainActivity.kt
@@ -12,7 +12,6 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
-import java.io.EOFException
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,14 +35,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         newsAdapter = NewsAdapter()
+        val newsService = retrofit.create(NewsService::class.java)
 
         binding.newsRecyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = newsAdapter
         }
 
-        val newsService = retrofit.create(NewsService::class.java)
-        newsService.mainFeed().enqueue(object: Callback<NewsRss> {
+        binding.feedChip.setOnClickListener {
+            binding.chipGroup.clearCheck() // 모든 칩들 isChecked를 해제시킴 (번거롭게 하나씩 지정하지 않아도 된다)
+            binding.feedChip.isChecked = true
+
+            newsService.mainFeed().submitList()
+        }
+
+        binding.politicsChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.politicsChip.isChecked = true
+
+            newsService.politicsNews().submitList()
+        }
+
+        binding.economyChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.economyChip.isChecked = true
+
+            newsService.economyNews().submitList()
+        }
+
+        binding.socialChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.socialChip.isChecked = true
+
+            newsService.socialNews().submitList()
+        }
+
+        binding.itChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.itChip.isChecked = true
+
+            newsService.itNews().submitList()
+        }
+
+        binding.sportsChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.sportsChip.isChecked = true
+
+            newsService.sportsNews().submitList()
+        }
+
+        newsService.mainFeed().submitList()
+    }
+
+    // 확장함수
+    private fun Call<NewsRss>.submitList() {
+        enqueue(object: Callback<NewsRss> {
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
                 Log.e("MainActivity", "${response.body()?.channel?.items}")
 
